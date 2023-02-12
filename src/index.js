25e49184e6c036eff6627956b7e60c4c1013b409
Olha Stanislavska <olga.stanislavskaya@gmail.com>
2023-02-12 19:33:14 +0200

    init index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10) {
+
+        let currentSymbolInBinary = expr.slice(i, i + 10);
+        let currentSymbolInMorse = '';
+
+        for (let k = 0; k < currentSymbolInBinary.length; k += 2) {
+            currentSymbolInMorse += currentSymbolInBinary.slice(k, k + 2) === '10' ? '.' 
+                : currentSymbolInBinary.slice(k, k + 2) === '11' ? '-'
+                : '';
+        }
+        result += MORSE_TABLE[currentSymbolInMorse] || ' ';
+    }
+    return result;
 }
 
 module.exports = {
